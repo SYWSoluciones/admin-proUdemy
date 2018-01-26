4e7c3f0c8836002fa78d234badc20c451c21219b
Edwin <softwareywebsoluciones@gmail.com>
2018-01-26 13:02:58 -0500

    modificaciones en los modulos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.route';
 
 @NgModule({
     declarations: [
@@ -19,7 +20,8 @@ import { SharedModule } from '../shared/shared.module';
         PagesComponent
     ],
     imports:[
-        SharedModule
+        SharedModule,
+        PAGES_ROUTES
     ]
   })
 
